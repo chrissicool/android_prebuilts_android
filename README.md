e7173b5317fbe12ddb490df444594bcd1d02da8d
Christian Ludwig <chrissicool@gmail.com>
2016-03-30 10:56:15 +0200

    Import first working AOSP prebuilts system
    This is the first version of a prebuilts system for AOSP. The major aim
    is to reduce compile-time during development. It also should not be in
    the way and should only need minimal maintenance.

---
+++ README.md
@@ -0,0 +1,77 @@
+# Android prebuilts system
+
+The Android prebuilts system lets you create pre-compiled packages for AOSP.
+It's main purpose is to speed up Android development.
+Compiling AOSP usually takes a very long time,
+especially for the first time.
+Although AOSP can make use of ccache for subsequent builds,
+compiling a fresh checkout usually takes ages.
+It gets even worse on not-so-beefy hardware.
+
+The Android prebuilts system tries to mitigate this.
+It provides prebuilt packages for heavy modules (in terms of compile time).
+It is aimed at developers for a fast edit-compile-run cycle.
+
+Currently this is a proof-of-concept with limitations.
+Look at the [Hacking](#hacking) section below for missing features.
+
+# Installation
+
+Add this repository to your (local) manifest
+and check it out to ```prebuilts/android```.
+
+Apply the following patch to ```build/core/main.mk```.
+
+```make
+--- a/core/main.mk
++++ b/core/main.mk
+@@ -527,6 +527,8 @@ ifneq ($(dont_bother),true)
+ subdir_makefiles := \
+        $(shell build/tools/findleaves.py --prune=$(OUT_DIR) --prune=.repo --prune=.git $(subdirs) Android.mk)
+ 
++-include $(TOP)/prebuilts/android/prebuilts.mk
++
+ $(foreach mk, $(subdir_makefiles), $(eval include $(mk)))
+ 
+ endif # dont_bother
+```
+# Usage
+
+Prebuilts are only enabled for engineering builds and userdebug builds.
+They are entirely disabled for user builds on purpose.
+
+On first-time use of the prebuilts system you need to popuate prebuilts.
+These will be used automatically in subsequent runs,
+even after you deleted the entire ```$OUT``` directory.
+
+To populate prebuilts, run
+
+```
+ $ m prebuilts
+```
+
+Prebuilts will be placed under ```$(TOP)/prebuilts/android/{target,host}```.
+Each target device and build variant has its own directory.
+The directory structure is chosen so that you can create separate repositories for the binaries.
+These repositories then can be used to prepopulate other build hosts via (local) manifest.
+
+It is recommended to re-populate prebuilts every now and then.
+Prebuilts will not be recompiled if not necessary.
+Prebuilts will not be used for modules that have another version than the prebuilt binary.
+
+# Hacking
+
+Yes, please!
+If you find an issue,
+open a bug report on Github.
+Fork this project on Github and send pull requests.
+
+This prebuilts system aims to be a drop-in solution for AOSP builds.
+Other than the on-line patch for the [installation](#installation),
+there should be no need to touch upstream code.
+
+Known TODOs are:
+
+ * implement proper multiarch (arm64) support
+ * implement support for host binaries
+ * prebuild all binaries
---
+++ prebuilts.mk
@@ -0,0 +1,96 @@
+# Copyright (C) 2016 Christian Ludwig <chrissicool@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check a number of conditions when _not_ to use prebuilts.
+# - CI builds (e.g. Jenkins)
+# - "user" build variant
+prebuilts_criteria := $(JENKINS_URL)$(filter user,$(TARGET_BUILD_VARIANT))
+ifneq ($(prebuilts_criteria),)
+ANDROID_BUILD_FROM_SOURCE := true
+endif # prebuilts_criteria
+prebuilts_criteria :=
+
+PREBUILTS_ROOT := $(call my-dir)
+PREBUILTS_BINARIES := $(PREBUILTS_ROOT)
+PREBUILTS_TARGET_BINARIES := $(PREBUILTS_BINARIES)/target/$(TARGET_DEVICE)/$(TARGET_BUILD_VARIANT)
+PREBUILTS_HOST_BINARIES := $(PREBUILTS_BINARIES)/host/$(HOST_PREBUILT_TAG)
+
+# Save a variable that contains a build system's makefile path.
+# Adjust the path to the makefile to $(PREBUILTS_ROOT).
+# The prebuilt makefile can use the original one as PREBUILTS_ORIGINAL_$(1).
+#
+# Parameters:
+#   $(1):	Makefile to adjust, e.g. BUILD_EXECUTABLE
+define save_var
+    $(eval PREBUILTS_ORIGINAL_$(1) := $($(1)))
+    $(eval $(1) := $(PREBUILTS_ROOT)/$(notdir $($(1))))
+    $(if $(wildcard $($(1))),,$(error Cannot find prebuilts override for $(1) = $($(1))))
+endef
+
+# Restore a variable formerly adjusted by save_var.
+#
+# Parameters:
+#   $(1):	Makefile to restore, e.g. BUILD_EXECUTABLE
+define restore_var
+    $(eval $(1) := $(PREBUILTS_ORIGINAL_$(1)))
+    $(eval $(PREBUILTS_ORIGINAL_$(1)) := )
+endef
+
+# Save and adjust all variables defined in prebuilts.definitions.mk.
+define save_vars
+    PREBUILTS_DEFINITIONS_MODE := save
+    include $(PREBUILTS_ROOT)/prebuilts.definitions.mk
+    PREBUILTS_DEFINITIONS_MODE :=
+endef
+
+# Restore all variables defined in prebuilts.definitions.mk.
+define restore_vars
+    PREBUILTS_DEFINITIONS_MODE := restore
+    include $(PREBUILTS_ROOT)/prebuilts.definitions.mk
+    PREBUILTS_DEFINITIONS_MODE :=
+endef
+
+prebuilts_projects := \
+    external/chromium_org \
+    external/llvm
+
+# Check each entry in $(prebuilts_projects) for existence in $(subdir_makefiles).
+prebuilts_makefiles :=
+$(foreach project, $(prebuilts_projects),\
+  $(if $(filter $(addsuffix /Android.mk, $(addprefix $(TOP)/, $(project))), $(subdir_makefiles)),\
+    $(eval prebuilts_makefiles += $(addsuffix /Android.mk, $(addprefix $(TOP)/, $(project)))),\
+    $(error Invalid prebuilt project specified: $(project))\
+  )\
+)
+
+ifneq ($(prebuilts_makefiles),)
+# Export a target that can be used to populate new prebuilts binaries.
+.PHONY: prebuilts
+prebuilts: ;
+
+$(eval $(call save_vars))
+$(foreach mk,\
+    $(prebuilts_makefiles),\
+    $(eval include $(mk))\
+    $(eval subdir_makefiles := $(filter-out $(mk), $(subdir_makefiles)))\
+)
+$(eval $(call restore_vars))
+endif # prebuilts_makefiles
+
+prebuilts_projects :=
+prebuilts_makefiles :=
+PREBUILTS_HOST_BINARIES :=
+PREBUILTS_TARGET_BINARIES :=
+PREBUILTS_BINARIES :=
+PREBUILTS_ROOT :=
